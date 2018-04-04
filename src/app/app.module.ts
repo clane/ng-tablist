c837b9c75aadb52dc28f5a635910cd39dd8f4ce5
Chris Lane <chris.lane359@gmail.com>
2018-04-04 14:20:12 -0700

    starting tablist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TablistComponent } from './tablist/tablist.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablistComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tablist/tablist.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tablist',
+  templateUrl: './tablist.component.html',
+  styleUrls: ['./tablist.component.css']
+})
+export class TablistComponent implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
