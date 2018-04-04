f987e29fde2812fea572b6d708bab6670460fcdd
Chris Lane <chris.lane359@gmail.com>
2018-04-04 14:39:57 -0700

    progress

--- src/app/tablist/tablist.component.ts
+++ src/app/tablist/tablist.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tablist.component.css']
 })
 export class TablistComponent implements OnInit {
-
   
+  currentTab: number;
+  tabData: { tab1: "tab 1 content ...", tab2: "tab 2 content ...",};
 
-  constructor() { }
+
+  constructor() {
+      this.currentTab = 0; 
+
+   }
 
   ngOnInit() {
   }
