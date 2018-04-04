edf885539ec3c1bc17788661d64cc54b8b641738
Chris Lane <chris.lane359@gmail.com>
2018-04-04 15:32:28 -0700

    now setting attributes with Renderer2

--- src/app/tablist/tablist.component.ts
+++ src/app/tablist/tablist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tablist',
@@ -9,8 +9,12 @@ export class TablistComponent implements OnInit {
   
 
   tabContent: string;
+  currentTab: number;
 
-  constructor() {
+  @ViewChild('tab1') tab1:ElementRef;
+  @ViewChild('tab2') tab2:ElementRef;
+
+  constructor(private renderer: Renderer2) {
       this.tabContent = "tab content will go here";
    }
 
@@ -18,11 +22,21 @@ export class TablistComponent implements OnInit {
   }
 
   setTabContent(tabNumber: number) {
+
+     this.currentTab = tabNumber;
+
     if(tabNumber === 1){
       this.tabContent = "Tab 1 Content Lorem impsum...";
+      this.renderer.setAttribute(this.tab1.nativeElement, 'tabindex', '0'); 
+      this.renderer.setAttribute(this.tab1.nativeElement, 'aria-selected', 'true'); 
+      this.renderer.setAttribute(this.tab1.nativeElement, 'aria-expanded', 'true'); 
+      
     }
     if(tabNumber === 2){
       this.tabContent = "Tab 2 Content Lorem impsum...";
+      this.renderer.setAttribute(this.tab2.nativeElement, 'tabindex', '0'); 
+      this.renderer.setAttribute(this.tab2.nativeElement, 'aria-selected', 'true'); 
+      this.renderer.setAttribute(this.tab2.nativeElement, 'aria-expanded', 'true'); 
     }
 
   }
